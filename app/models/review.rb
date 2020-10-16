b26315044f939445d07da3ec8561dfb2b4532148
donnymays <dtmays2@gmail.com>
2020-10-16 09:41:10 -0700

    PASS spec validations for review class

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, presence: true
+  validates :content, length: { in: 50..250 }
+  validates :rating, numericality: { only_integer: true, less_than: 6 }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 describe Review do
   it { should belong_to(:product) }
+  it { should validate_length_of(:content) }
+  it { should validate_presence_of(:author) }
+  it { should validate_numericality_of(:rating) }
 end
