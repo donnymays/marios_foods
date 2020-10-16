ebf89f9a4686a3728aa9dc91a83c301673f9ef99
donnymays <dtmays2@gmail.com>
2020-10-16 10:22:31 -0700

    debugging Faker methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+Review.destroy_all
+
+50.times do |index|
+  product = Product.create!(name: Faker::Food.ingredient, cost: Faker::Commerce.price, country_of_origin: Faker::TvShows::GameOfThrones.city)
+end
+  
+  
+250.times do |index|
+  review = Review.create!(author: Faker::Movies::Lebowski.character, content: Faker::Lorem.characters(number: 150), rating: rand(6), product_id: rand(51))
+end
