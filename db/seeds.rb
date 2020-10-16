f5414903cd9adaff882675516b0eb8c1b772553b
donnymays <dtmays2@gmail.com>
2020-10-16 10:46:35 -0700

    faker db poplulated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,15 @@
 Product.destroy_all
 Review.destroy_all
 
+
+
+
 50.times do |index|
   product = Product.create!(name: Faker::Food.ingredient, cost: Faker::Commerce.price, country_of_origin: Faker::TvShows::GameOfThrones.city)
-end
+end  
   
-  
+product_random_id = Product.all.ids.sample
+
 250.times do |index|
-  review = Review.create!(author: Faker::Movies::Lebowski.character, content: Faker::Lorem.characters(number: 150), rating: rand(6), product_id: rand(51))
+  review = Review.create!(author: Faker::Movies::Lebowski.character, content: Faker::Lorem.characters(number: 150), rating: rand(6), product_id: product_random_id)
 end
