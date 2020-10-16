c9c4ebc483c7ccdec8cc9af5a7876b56681d4fb5
donnymays <dtmays2@gmail.com>
2020-10-16 09:46:34 -0700

    PASS spec product_titleize method

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,4 +5,8 @@ describe Product do
   it { should validate_presence_of :name }
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
+  it("titleizes the name of an product") do
+    product = Product.create({name: "venison", cost: 800, country_of_origin: "Canada"})
+    expect(product.name()).to(eq("Venison"))
+  end
 end
