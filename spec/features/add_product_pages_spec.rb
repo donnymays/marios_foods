e09e518179257fe63ec99835d809f8c9618288e7
donnymays <dtmays2@gmail.com>
2020-10-23 12:35:51 -0700

    figuring out warden

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe "the add a product process" do
   it "adds a new product" do
     user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd', admin: true)
-    login_as(user, :scope => :user)
-    visit products_path
+    visit new_user_registration_path
+    login_as(user, :scope => :admin)
     click_link 'Create new product'
     fill_in 'Name', :with => 'Kangaroo'
     fill_in 'Cost', :with => '3'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,5 +61,13 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  config.include Warden::Test::Helpers
+  config.include Warden::Test::Helpers, type: :feature
+  Warden.test_mode!
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
