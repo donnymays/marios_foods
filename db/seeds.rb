72b3ecbe0158c0cc94e5852f8222a308235e385e
donnymays <dtmays2@gmail.com>
2020-10-23 08:56:23 -0700

    seed db with user and admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,12 @@ Review.destroy_all
   product = Product.create!(name: Faker::Food.ingredient, cost: Faker::Commerce.price, country_of_origin: Faker::TvShows::GameOfThrones.city)
 end  
   
-
-
 250.times do |index|
   product_random_id = Product.all.ids.sample
   review = Review.create!(author: Faker::Movies::Lebowski.character, content: Faker::Hipster.paragraph_by_chars(characters: 150, supplemental: false), rating: rand(6), product_id: product_random_id)
+end
+
+1.times do |index|
+  user = User.create!(email: 'user@user.com', password: 'user123', admin: false )
+  admin = User.create!(email: 'admin@admin.com', password: 'admin123', admin: true)
 end
